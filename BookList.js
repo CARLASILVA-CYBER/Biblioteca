67357896033d7b7b2f134859230da8fc6ecd88ac
Carla Silva <61299273+CARLASILVA-CYBER@users.noreply.github.com>
2024-10-31 18:13:23 +0000

    Create BookList.js

---
+++ BookList.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function BookList() {
+    const [books, setBooks] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:5000/books')
+            .then(response => setBooks(response.data))
+            .catch(error => console.error("Erro ao buscar livros:", error));
+    }, []);
+
+    return (
+        <div>
+            <h2>Livros Disponíveis</h2>
+            <ul>
+                {books.map(book => (
+                    <li key={book.id}>{book.titulo} - {book.autor}</li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default BookList;
+
